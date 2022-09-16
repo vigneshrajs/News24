d53d1b60757391b4294603215fd48db72410a58d
vigneshrajs <vigneshraj.s@tringapps.com>
2022-09-16 16:02:45 +0530

    config files added

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(false);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            'src/*': './src',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+    env: {
+      production: {
+        plugins: ["transform-remove-console"],     //removing consoles.log from app during release (production) versions
+      },
+    },
+  };
 };
